3372fd9c23a517effe19792fbad8e65b871cb874
Nicholas Shephard <shephardn@gmail.com>
2021-11-02 12:49:31 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// 1.
+var customerName = 'bob';
+// console.log(customerName);
+// console.log(customerName.toUpperCase());
+
+// 2.
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+// 3.
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// 4.
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// 5.
+const leastFavoriteCustomer = 'nick';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+}
